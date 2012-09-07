8168d383fdd480ff404c9c36cbb2345bb9be5c80
Wojtek <wojciech.kotlarski@gmail.com>
2012-09-07 23:38:39 +0100

    added license/author headers

--- Spring.FluentContext/FluentApplicationContext.cs
+++ Spring.FluentContext/FluentApplicationContext.cs
@@ -1,3 +1,30 @@
+//
+//  Author:
+//    Wojciech Kotlarski wojciech.kotlarski@gmail.com
+//
+//  Copyright (c) 2012, Wojciech Kotlarski
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.	 
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in
+//       the documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+//  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
 using Spring.Context;
 using Spring.Context.Support;
 using Spring.FluentContext.BuildingStages;
@@ -9,13 +36,27 @@ namespace Spring.FluentContext
 {
 	public class FluentApplicationContext : GenericApplicationContext
 	{
-		public FluentApplicationContext() { }
-		public FluentApplicationContext(bool caseSensitive) : base(caseSensitive) { }
-		public FluentApplicationContext(DefaultListableObjectFactory objectFactory) : base(objectFactory) { }
-		public FluentApplicationContext(IApplicationContext parent) : base(parent) { }
-		public FluentApplicationContext(string name, bool caseSensitive, IApplicationContext parent) : base(name, caseSensitive, parent) { }
-		public FluentApplicationContext(DefaultListableObjectFactory objectFactory, IApplicationContext parent) : base(objectFactory, parent) { }
-		public FluentApplicationContext(string name, bool caseSensitive, IApplicationContext parent, DefaultListableObjectFactory objectFactory) : base(name, caseSensitive, parent, objectFactory) { }
+		public FluentApplicationContext()
+		{
+		}
+		public FluentApplicationContext(bool caseSensitive) : base(caseSensitive)
+		{
+		}
+		public FluentApplicationContext(DefaultListableObjectFactory objectFactory) : base(objectFactory)
+		{
+		}
+		public FluentApplicationContext(IApplicationContext parent) : base(parent)
+		{
+		}
+		public FluentApplicationContext(string name, bool caseSensitive, IApplicationContext parent) : base(name, caseSensitive, parent)
+		{
+		}
+		public FluentApplicationContext(DefaultListableObjectFactory objectFactory, IApplicationContext parent) : base(objectFactory, parent)
+		{
+		}
+		public FluentApplicationContext(string name, bool caseSensitive, IApplicationContext parent, DefaultListableObjectFactory objectFactory) : base(name, caseSensitive, parent, objectFactory)
+		{
+		}
 
 		public IScopeBuildStage<T> RegisterNamed<T>(string id)
 		{
